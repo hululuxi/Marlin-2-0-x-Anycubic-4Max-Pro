97ca908e1df224053f64951b9a1cb25d939d84ea
hululuxi <35184768+hululuxi@users.noreply.github.com>
2024-07-13 19:39:22 +0800

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_TRIGORILLA_14
+  #define MOTHERBOARD BOARD_RAMPS_14_EFB
 #endif
 
 /**
